9970da414a9058ba17262738ef573d9d20e5896f
nant0313 <nant0313@gmail.com>
2021-08-25 22:01:12 +0900

    Add tests & update const variables

--- test/channel_test.go
+++ test/channel_test.go
@@ -1,24 +1,24 @@
 package test
 
 import (
+	checkupdate "checkUpdate"
+	"fmt"
 	"testing"
 	"updateDB"
-
-	"github.com/slack-go/slack"
 )
 
 var boardTypes = []string{
-	"general", "grad", "job", "main", "sgcs", "underg",
+	"cs-main", "cs-underg", "cs-grad", "cs-general", "cs-applause", "cs-job", "scholar", "underg", "general",
 }
 
 func TestChannel(t *testing.T) {
-	lists := updateDB.GetChannels(boardTypes)
+	lists := updateDB.FindChannels(boardTypes)
 	dummy1 := updateDB.ChannelWrap{
-		ChannelID: "C011WQ31WF2",
+		ChannelID: "C01MSUXUY3E",
 		TeamToken: "xoxb-560811458228-1743020221089-lB7Cw0i7ReRCJJ6zzkN8G1kY",
 	}
 	dummy2 := updateDB.ChannelWrap{
-		ChannelID: "U011D7BSBAR",
+		ChannelID: "C01MSUXUY3E",
 		TeamToken: "xoxb-560811458228-1743020221089-lB7Cw0i7ReRCJJ6zzkN8G1kY",
 	}
 	toCompare := []updateDB.ChannelWrap{dummy1, dummy2}
@@ -30,6 +30,13 @@ func TestChannel(t *testing.T) {
 	}
 	t.Fail()
 }
+func TestArticleCount(t *testing.T) {
+	boards := updateDB.FindBoards()
+	for _, board := range boards {
+		updated := checkupdate.FindUpdatedArticle(board.Link, board.IsCsBoard, board.LastNotified)
+		fmt.Print(board.Name, len(updated))
+	}
+}
 
 /*func TestMessage(t *testing.T) {
 	var api = slack.New("xoxb-560811458228-1743020221089-lB7Cw0i7ReRCJJ6zzkN8G1kY")
@@ -38,7 +45,7 @@ func TestChannel(t *testing.T) {
 	api.PostMessage("U011D7BSBAR", slack.MsgOptionAttachments(attch...))
 }*/
 
-func TestBroadcast(t *testing.T) {
+/*func TestBroadcast(t *testing.T) {
 	check := make(map[string]bool)
 	lists := updateDB.GetChannels(boardTypes)
 	for _, elem := range lists {
@@ -52,4 +59,4 @@ func TestBroadcast(t *testing.T) {
 		attch = append(attch, slack.Attachment{Pretext: "Notify SGCS Bot 점검 안내", Title: "25일(수) 오후 1시부터 오후 6시까지 점검이 예정되어 있습니다.\r이용에 참고 부탁드립니다."})
 		api.PostMessage(elem.ChannelID, slack.MsgOptionAttachments(attch...))
 	}
-}
+}*/

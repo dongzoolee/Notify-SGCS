0dd5ee52d9f6e59cc1af4fb36892c7f06b1058e8
nant0313 <nant0313@gmail.com>
2021-08-25 21:56:27 +0900

    Change function name

--- broadcast/broadcast.go
+++ broadcast/broadcast.go
@@ -12,16 +12,17 @@ var boardTypes = []string{
 
 func Broadcast() {
 	check := make(map[string]bool)
-	lists := updateDB.GetChannels(boardTypes)
+	lists := updateDB.FindChannels(boardTypes)
 	for _, elem := range lists {
 		if check[elem.ChannelID] {
 			continue
 		}
 		check[elem.ChannelID] = true
-		
+
 		var api = slack.New(elem.TeamToken)
 		var attch []slack.Attachment
 		attch = append(attch, slack.Attachment{Pretext: "Notify SGCS Bot 점검 안내", Title: "25일(수) 오후 1시부터 오후 6시까지 점검이 예정되어 있습니다.\r이용에 참고 부탁드립니다."})
-		api.PostMessage(elem.ChannelID, slack.MsgOptionAttachments(attch...))
+		_, _, err := api.PostMessage(elem.ChannelID, slack.MsgOptionAttachments(attch...))
+		updateDB.ErrCheck(err)
 	}
 }
